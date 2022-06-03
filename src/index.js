2134c3b2860307e7eef9bb5dc02f0172b9c68f21
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-06-03 09:44:02 -0300

    feat: simple register page and route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import Register from './Register/Register';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <App /> } />
+        <Route path="/register" element={ <Register /> } />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
