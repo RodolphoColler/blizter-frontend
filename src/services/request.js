089574eacae0a364323299d88b10ea560ea5ca69
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-06-14 10:25:57 -0300

    feat: add token to default axios request

--- src/services/request.js
+++ src/services/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.headers.common.Authorization = localStorage.getItem('token');
 
 export async function login(body) {
   try {
