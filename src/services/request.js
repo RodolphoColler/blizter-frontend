e4c528cff0b992467c797ca0f011162ad6c0f664
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-06-15 12:09:09 -0300

    feat: create Expenditure to add some new expenditure on db

--- src/services/request.js
+++ src/services/request.js
@@ -22,3 +22,25 @@ export async function register(body) {
     throw new Error(error.response.data.message);
   }
 }
+
+export async function expenditure(body) {
+  try {
+    const { data } = await axios.post('/expenditure', body);
+
+    return data;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+}
+
+export async function getCategories() {
+  const { data: { categories } } = await axios.get('/category');
+
+  return categories;
+}
+
+export async function getUserId() {
+  const { data: { id } } = await axios.get('/token');
+
+  return id;
+}
