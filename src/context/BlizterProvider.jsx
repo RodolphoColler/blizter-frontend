e18793029d8fe822454dd5b9aa0e6cf83a0ec83c
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-06-21 09:36:56 -0300

    feat: add date to provider

--- src/context/BlizterProvider.jsx
+++ src/context/BlizterProvider.jsx
@@ -1,9 +1,18 @@
+import moment from 'moment';
 import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 import BlizterContext from './BlizterContext';
 
 function BlizterProvider({ children }) {
+  const [date, setDate] = useState('');
+  const [dateCount, setDateCount] = useState(0);
+
+  useEffect(() => {
+    setDate(moment().subtract(dateCount, 'month').format('DD MMMM YYYY'));
+  }, [dateCount]);
+
   return (
-    <BlizterContext.Provider value={ { } }>
+    <BlizterContext.Provider value={ { date, setDateCount } }>
       {children}
     </BlizterContext.Provider>
   );
