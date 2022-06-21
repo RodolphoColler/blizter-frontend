c4b6d1474c5d9b472d816b63f8bec7cd3300b662
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-06-21 09:36:42 -0300

    feat: simple data shower and setter

---
+++ src/ShowDate/ShowDate.jsx
@@ -0,0 +1,28 @@
+import { useContext } from 'react';
+import { MdArrowForwardIos } from 'react-icons/md';
+import BlizterContext from '../context/BlizterContext';
+import './ShowDate.css';
+
+function ShowDate() {
+  const { date, setDateCount } = useContext(BlizterContext);
+
+  return (
+    <div className="date-container">
+      <button
+        type="button"
+        onClick={ () => setDateCount((prev) => prev + 1) }
+      >
+        <MdArrowForwardIos height="20px" />
+      </button>
+      <h2>{date}</h2>
+      <button
+        type="button"
+        onClick={ () => setDateCount((prev) => prev - 1) }
+      >
+        <MdArrowForwardIos />
+      </button>
+    </div>
+  );
+}
+
+export default ShowDate;
