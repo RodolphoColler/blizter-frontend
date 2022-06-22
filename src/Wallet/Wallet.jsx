843887e03b7cddb2d8723db094d95eee99d5286e
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-06-22 15:52:24 -0300

    feat: wallet page adding changes

--- src/Wallet/Wallet.jsx
+++ src/Wallet/Wallet.jsx
@@ -3,9 +3,17 @@ import ExpenditureForm from '../ExpenditureForm/ExpenditureForm';
 import { getUserCategories } from '../services/request';
 import Expenditures from '../Expenditures';
 import './Wallet.css';
+import Header from '../Header';
+import SalaryForm from '../SalaryForm';
+import ShowDate from '../ShowDate';
+import BarChart from '../BarChart';
+import DoughnutChartSalary from '../DoughnutChartSalary';
+import DoughnutChartExpenses from '../DoughnutChartExpenses';
+import ShowSalary from '../ShowSalary/ShowSalary';
 
 function Wallet() {
   const [isFormVisible, setIsFormVisible] = useState(false);
+  const [isSalaryFormVisible, setIsSalaryFormVisible] = useState(false);
   const [userCategories, setUserCategories] = useState([]);
 
   useEffect(() => {
@@ -15,27 +23,43 @@ function Wallet() {
   return (
     <>
       {
-        isFormVisible && (
-          <ExpenditureForm
-            setIsFormVisible={ setIsFormVisible }
-            userCategories={ userCategories }
-            setUserCategories={ setUserCategories }
-          />
+        isSalaryFormVisible && (
+          <SalaryForm setIsSalaryFormVisible={ setIsSalaryFormVisible } />
         )
       }
       <main className="wallet-page">
-        <aside>
-          <button
-            type="button"
-            className="create-expend-button"
-            onClick={ () => { setIsFormVisible(!isFormVisible); } }
-          >
-            Create Expend
-          </button>
-          <Expenditures userCategories={ userCategories } isFormVisible={ isFormVisible } />
-        </aside>
-        <div>
-          <h1>In building</h1>
+        <Header />
+        <div className="wallet-page-content">
+          <aside>
+            {
+              isFormVisible && (
+                <ExpenditureForm
+                  setIsFormVisible={ setIsFormVisible }
+                  userCategories={ userCategories }
+                  setUserCategories={ setUserCategories }
+                />
+              )
+            }
+            <button
+              type="button"
+              className="create-expend-button"
+              onClick={ () => { setIsFormVisible(!isFormVisible); } }
+            >
+              Create Expend
+            </button>
+            <Expenditures userCategories={ userCategories } isFormVisible={ isFormVisible } />
+          </aside>
+          <div className="charts-container">
+            <div className="chart-header">
+              <ShowSalary setIsSalaryFormVisible={ setIsSalaryFormVisible } />
+              <ShowDate />
+            </div>
+            <div className="doughnuts-container">
+              <DoughnutChartSalary />
+              <DoughnutChartExpenses />
+            </div>
+            <BarChart />
+          </div>
         </div>
       </main>
     </>
