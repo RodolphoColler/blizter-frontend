e01450442811f50ce6fdea534ffc4f4484120db0
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-06-22 15:48:57 -0300

    feat: add salay request to provider

--- src/context/BlizterProvider.jsx
+++ src/context/BlizterProvider.jsx
@@ -1,18 +1,26 @@
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
+import { getSalary } from '../services/request';
 import BlizterContext from './BlizterContext';
 
 function BlizterProvider({ children }) {
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState(moment().format('DD MMMM YYYY'));
   const [dateCount, setDateCount] = useState(0);
+  const [salary, setSalary] = useState(1);
 
   useEffect(() => {
     setDate(moment().subtract(dateCount, 'month').format('DD MMMM YYYY'));
   }, [dateCount]);
 
+  useEffect(() => {
+    getSalary(date)
+      .then((data) => setSalary(data.value))
+      .catch(() => setSalary(0));
+  }, [date]);
+
   return (
-    <BlizterContext.Provider value={ { date, setDateCount } }>
+    <BlizterContext.Provider value={ { date, setDateCount, salary, setSalary } }>
       {children}
     </BlizterContext.Provider>
   );
