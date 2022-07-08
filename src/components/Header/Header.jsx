e6e07e019dba6498cc770d0e9424a7fa72cf33f1
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-07-08 09:32:07 -0300

    feat: create page indicator

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,29 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../icons/lotus.svg';
 import './Header.scss';
 
 function Header() {
+  const { pathname } = useLocation();
+
   return (
     <header className="wallet-header">
       <Link to="/home" className="blizter-logo">
         <Logo fill="#2d7dc8" width="33px" />
         <p>Blizter</p>
       </Link>
-      <ul>
-        <Link to="/About"><li>About</li></Link>
-        <Link to="/Home"><li>Home</li></Link>
-      </ul>
+      <nav>
+        <ul>
+          <li className={ `nav-link${pathname === '/about' ? '-selected' : ''}` }>
+            <Link to="/about">About</Link>
+          </li>
+          <li className={ `nav-link${pathname === '/home' ? '-selected' : ''}` }>
+            <Link to="/home">Home</Link>
+          </li>
+          <li className={ `nav-link${pathname === '/dashboard' ? '-selected' : ''}` }>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
