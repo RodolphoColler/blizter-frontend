acf5d68f943d4c250d7cb8a479972fba8ce84957
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-07-07 22:58:17 -0300

    style: better responsivite to dashboard page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../icons/lotus.svg';
-import './Header.css';
+import './Header.scss';
 
 function Header() {
   return (
     <header className="wallet-header">
       <Link to="/home" className="blizter-logo">
-        <Logo fill="#337bbe" width="35px" />
+        <Logo fill="#2d7dc8" width="33px" />
         <p>Blizter</p>
       </Link>
       <ul>
