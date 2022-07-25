afb374e087c7ef9b9015491f9a5b81979ee5b1e0
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-07-24 22:35:24 -0300

    feat: starting about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,19 @@
+import { Header } from '../../components';
+import './About.scss';
+
 function About() {
   return (
-    <h1>In Building</h1>
+    <main className="about-page">
+      <Header />
+      <section className="about-content">
+        <p>
+          This project born when I made a simple full-stack todo list, I really enjoy the first experience
+          integrating the front and the back-end but I wanted to do something more ambitious. I started to
+          think but nothing came out, until I found a image of an dashboard, at this point the ideas
+          started to ge a shape.
+        </p>
+      </section>
+    </main>
   );
 }
 
