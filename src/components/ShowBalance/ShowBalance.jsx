89c433dc6d659b7cb0a3d166a396778119225104
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-08-09 10:21:34 -0300

    refact Show balance form to use scss

--- src/components/ShowBalance/ShowBalance.jsx
+++ src/components/ShowBalance/ShowBalance.jsx
@@ -2,20 +2,24 @@ import PropTypes from 'prop-types';
 import { useEffect, useContext, useState } from 'react';
 import BlizterContext from '../../context/BlizterContext';
 import { getMonthExpenditures } from '../../services/request';
-import './ShowBalance.css';
+import './ShowBalance.scss';
 
 function ShowBalance({ setIsSalaryFormVisible }) {
   const [monthExpenditures, setMonthExpenditures] = useState(0);
-  const { salary, date, isExpenditureFormVisible } = useContext(BlizterContext);
+  const { salary, date, isExpenditureFormVisible, setIsSignedModalVisible } = useContext(BlizterContext);
+
+  async function fetchMonthExpenditures() {
+    try {
+      const expenditures = await getMonthExpenditures(date);
+      const expendituresSum = expenditures.reduce((acc, cur) => acc + cur.sum, 0);
+      setMonthExpenditures(expendituresSum);
+    } catch ({ message }) {
+      if (message.includes('token')) setIsSignedModalVisible(true);
+    }
+  }
 
   useEffect(() => {
-    if (!isExpenditureFormVisible) {
-      getMonthExpenditures(date)
-        .then((data) => {
-          const result = data.reduce((acc, cur) => acc + cur.sum, 0);
-          setMonthExpenditures(result);
-        });
-    }
+    if (!isExpenditureFormVisible) fetchMonthExpenditures();
   }, [date, isExpenditureFormVisible]);
 
   useEffect(() => { if (!salary) setIsSalaryFormVisible(true); }, [salary]);
