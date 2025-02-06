3c0508f2b29c9028a99c2af48a4d74b075534ccb
RodolphoColler <rodolpho.coller12@gmail.com>
2025-02-06 13:15:28 -0300

    starting tests

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { BsArrowRightShort } from 'react-icons/bs';
 import { Header } from '../../components';
 import './Home.scss';
 
+async function callApi() {
+  const baseApiUrl = process.env.REACT_APP_API_URL;
+
+  const response = await axios.get(baseApiUrl);
+
+  return response.data.message;
+}
+
 function Home() {
+  const [message, setMessage] = useState('f');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await callApi();
+      setMessage(result);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <main className="home-page">
       <Header />
@@ -12,6 +33,7 @@ function Home() {
         <div className="text-container">
           <p>Easy and better way to get in day with your financial life</p>
           <p>Blizter is your everyday life wallet made to be minimalist and simple to use</p>
+          <p>{message}</p>
           <Link to="/signin">
             Let&apos;s get started
             <BsArrowRightShort fontSize="23px" />
